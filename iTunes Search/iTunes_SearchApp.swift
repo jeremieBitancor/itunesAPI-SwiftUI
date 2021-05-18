db33b05783d636e22e8ad632aa844920d3fb7fbc
jeremie bitancor <bitancor.jeremie@gmail.com>
2021-05-18 20:19:36 +0800

    Initial Commit

---
+++ iTunes Search/iTunes_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunes_SearchApp.swift
+//  iTunes Search
+//
+//  Created by jeremie bitancor on 5/18/21.
+//
+
+import SwiftUI
+
+@main
+struct iTunes_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
